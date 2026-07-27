2885b94de14e656a698b5e1635a78e3d2c94b380
nicvicbdynamites <nicvicb@integrationsatnicvicbdynamites.com>
2026-07-27 07:45:50 -0400

    index.ts

--- artifacts/api-server/src/index.ts
+++ artifacts/api-server/src/index.ts
@@ -1,25 +1,15 @@
 import app from "./app";
 import { logger } from "./lib/logger";
 
-const rawPort = process.env["PORT"];
+// Provide default fallback port (8080) if PORT env variable is omitted
+const port = Number(process.env.PORT) || 8080;
+const host = "0.0.0.0";
 
-if (!rawPort) {
-  throw new Error(
-    "PORT environment variable is required but was not provided.",
-  );
-}
-
-const port = Number(rawPort);
-
-if (Number.isNaN(port) || port <= 0) {
-  throw new Error(`Invalid PORT value: "${rawPort}"`);
-}
-
-app.listen(port, (err) => {
+app.listen(port, host, (err?: any) => {
   if (err) {
     logger.error({ err }, "Error listening on port");
     process.exit(1);
   }
 
-  logger.info({ port }, "Server listening");
+  logger.info({ port, host }, "Server listening");
 });
